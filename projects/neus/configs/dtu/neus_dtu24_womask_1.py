81226ba8f4ecee8412802c171cb17643f26088a9
chaochao42 <2504483651@qq.com>
2022-11-24 10:34:53 +0800

    update .gitignore

--- projects/neus/configs/dtu/neus_dtu24_womask_1.py
+++ projects/neus/configs/dtu/neus_dtu24_womask_1.py
@@ -77,7 +77,7 @@ optim = dict(
     betas=(0.9,0.99),
 )
 
-base_exp_dir = './log/dtu_scan24/womask_1'
+base_exp_dir = '../log/dtu_scan24/womask_1'
 recording = [ './','./models']
 
 learning_rate_alpha = 0.05
