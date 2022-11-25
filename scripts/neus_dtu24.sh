80db79fd4300eb90408c7c3c20359b581276d2b3
chaochao42 <2504483651@qq.com>
2022-11-25 17:36:40 +0800

    longer longer experiments

--- scripts/neus_dtu24.sh
+++ scripts/neus_dtu24.sh
@@ -1,3 +1,3 @@
 export CUDA_VISIBLE_DEVICES=2
 
-python ../tools/run_net.py --config-file ../projects/neus/configs/dtu/neus_dtu24_womask_2.py --type mesh --task train
+python ../tools/run_net.py --config-file ../projects/neus/configs/dtu/neus_dtu24_womask_3.py --type mesh --task train
