fc9fecccc14c157c52da553bcd228d63ec04e401
chaochao42 <2504483651@qq.com>
2022-12-02 15:15:03 +0800

    global commit 3

--- python/jnerf/models/losses/global_loss.py
+++ python/jnerf/models/losses/global_loss.py
@@ -1,14 +1,17 @@
 import jittor as jt
 from jittor import nn
 from jnerf.utils.registry import LOSSES
+import random
+import numpy as np
+
 
 @LOSSES.register_module()
 class GlobalLoss(nn.Module):
-    def __init__(self, delta=1):
-        self.delta = delta
+    def __init__(self, shot_number=4, pose_number=35):
+        self.shot_number = shot_number
+        self.pose_number = pose_number
     def execute(self, poses):
-
-        numbers = len(poses)
-
-        pose_list = jt.misc.chunk(poses, numbers, 0)
-        print(f"========={poses.shape}======={numbers}=========Length {len(pose_list)}====index 0 shape{pose_list[0].shape}")
+        index_list = np.random.randint(0, self.pose_number, size=self.shot_number)
+        pose_var = poses[index_list]
+        pose_list = jt.misc.chunk(pose_var, self.shot_number, 0)
+        relative_1 = 
