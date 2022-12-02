838c4f1793424decd5c773ab674ab7f90ffdfb38
chaochao42 <2504483651@qq.com>
2022-12-02 15:29:30 +0800

    global commit 4

--- python/jnerf/models/losses/global_loss.py
+++ python/jnerf/models/losses/global_loss.py
@@ -10,8 +10,14 @@ class GlobalLoss(nn.Module):
     def __init__(self, shot_number=4, pose_number=35):
         self.shot_number = shot_number
         self.pose_number = pose_number
+
+    # def pose_to_angle(self, single_pose):
+
     def execute(self, poses):
         index_list = np.random.randint(0, self.pose_number, size=self.shot_number)
         pose_var = poses[index_list]
         pose_list = jt.misc.chunk(pose_var, self.shot_number, 0)
-        relative_1 = 
+        print(f"============ pose_list type {type(pose_list)} and length {len(pose_list)}")
+        print("========================================")
+        print(pose_list[0])
+        #relative_1 =
