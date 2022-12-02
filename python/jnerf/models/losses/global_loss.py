77ed1e666f7a5f481add33305603f7721bae95c9
chaochao42 <2504483651@qq.com>
2022-12-02 20:40:30 +0800

    global loss 5

--- python/jnerf/models/losses/global_loss.py
+++ python/jnerf/models/losses/global_loss.py
@@ -11,28 +11,32 @@ class GlobalLoss(nn.Module):
         self.shot_number = shot_number
         self.pose_number = pose_number
 
-    def pose_to_angle(self, single_pose):
-        print("=================== Single pose")
-        # print(f"======Shape {single_pose.shape}")
-        # print(single_pose)
-        r32 = single_pose[:,2,1]
-        r33 = single_pose[:,2,2]
-        r31 = single_pose[:,2,0]
-        r21 = single_pose[:,1,0]
-        r11 = single_pose[:,0,0]
+    def pose_to_angle(self, pose_var):
+        r32 = pose_var[:,2,1]
+        r33 = pose_var[:,2,2]
+        r31 = pose_var[:,2,0]
+        r21 = pose_var[:,1,0]
+        r11 = pose_var[:,0,0]
         theta_x = jt.misc.arctan2(r32, r33)
         theta_y = jt.misc.arctan2(-r31, jt.sqrt(r32**2 + r33**2))
         theta_z = jt.misc.arctan2(r21, r11)
+        return theta_x, theta_y, theta_z
 
-        print(f"=={theta_x}=={theta_y}=={theta_z}==")
-        # r32 = single_pose[]
+
     def execute(self, poses):
         index_list = np.random.randint(0, self.pose_number, size=self.shot_number)
         pose_var = poses[index_list]
-        pose_list = jt.misc.chunk(pose_var, self.shot_number, 0)
-        pose_0 = pose_list[0]
-        self.pose_to_angle(pose_0)
-        # print(f"============ pose_list type {type(pose_list)} and length {len(pose_list)}")
-        # print("========================================")
-        # print(pose_list[0])
+
+        theta_x, theta_y, theta_z = self.pose_to_angle(pose_var)
+
+        index_x, value_x = jt.argsort(theta_x)
+        index_y, value_y = jt.argsort(theta_y)
+        index_z, value_z = jt.argsort(theta_z)
+        print("===================================")
+        print(f"Index x==============")
+        print(index_x, value_x)
+        print(f"Index y==============")
+        print(index_y, value_y)
+        print(f"Index z==============")
+        print(index_z, value_z)
         #relative_1 =
