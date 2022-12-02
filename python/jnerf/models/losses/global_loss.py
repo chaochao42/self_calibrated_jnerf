5fa65ca150db3d7a6363105f403675d2c9222f16
chaochao42 <2504483651@qq.com>
2022-12-02 16:25:30 +0800

    global loss commit add new func

--- python/jnerf/models/losses/global_loss.py
+++ python/jnerf/models/losses/global_loss.py
@@ -11,13 +11,28 @@ class GlobalLoss(nn.Module):
         self.shot_number = shot_number
         self.pose_number = pose_number
 
-    # def pose_to_angle(self, single_pose):
+    def pose_to_angle(self, single_pose):
+        print("=================== Single pose")
+        # print(f"======Shape {single_pose.shape}")
+        # print(single_pose)
+        r32 = single_pose[:,2,1]
+        r33 = single_pose[:,2,2]
+        r31 = single_pose[:,2,0]
+        r21 = single_pose[:,1,0]
+        r11 = single_pose[:,0,0]
+        theta_x = jt.misc.arctan2(r32, r33)
+        theta_y = jt.misc.arctan2(-r31, jt.sqrt(r32**2 + r33**2))
+        theta_z = jt.misc.arctan2(r21, r11)
 
+        print(f"=={theta_x}=={theta_y}=={theta_z}==")
+        # r32 = single_pose[]
     def execute(self, poses):
         index_list = np.random.randint(0, self.pose_number, size=self.shot_number)
         pose_var = poses[index_list]
         pose_list = jt.misc.chunk(pose_var, self.shot_number, 0)
-        print(f"============ pose_list type {type(pose_list)} and length {len(pose_list)}")
-        print("========================================")
-        print(pose_list[0])
+        pose_0 = pose_list[0]
+        self.pose_to_angle(pose_0)
+        # print(f"============ pose_list type {type(pose_list)} and length {len(pose_list)}")
+        # print("========================================")
+        # print(pose_list[0])
         #relative_1 =
