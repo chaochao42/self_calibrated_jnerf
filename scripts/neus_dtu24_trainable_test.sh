9aa542e1f54bbef7eeea543504a87ca9127c8c04
chaochao42 <2504483651@qq.com>
2022-12-05 22:01:13 +0800

    new log

--- scripts/neus_dtu24_trainable_test.sh
+++ scripts/neus_dtu24_trainable_test.sh
@@ -1,3 +1,3 @@
 export CUDA_VISIBLE_DEVICES=3
 
-python ../tools/run_net.py --config-file ../projects/neus/configs/dtu_trainable/neus_dtu24_womask_global_0.py --type self_calibrated --task train
+python ../tools/run_net.py --config-file ../projects/neus/configs/dtu_trainable/neus_dtu24_womask_global_eik_0.py --type self_calibrated --task train
